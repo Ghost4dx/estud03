7c4c4d3f4bca48db6b200eb856c09749065a69b4
Ghost4dx <Daniel.boaes.18@gmail.com>
2024-09-17 10:03:21 -0300

    Add files via upload

---
+++ src/Main/main.java
@@ -0,0 +1,14 @@
+package Main;
+
+import Usuarios.*;
+import Itens.*;
+import java.util.Date;
+
+public class main {
+	
+	public static void main(String[] args) {
+		Sistema s = new Sistema();
+		s.menu();
+		
+	}
+}
